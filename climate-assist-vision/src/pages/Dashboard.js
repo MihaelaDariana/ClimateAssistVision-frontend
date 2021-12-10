68caa8f9c506112260133fa1d2fb7a79a80030a2
MihaelaDariana <pop_mihaela_dariana@yahoo.com>
2021-12-10 23:08:52 +0200

    change API, added weather on current location

--- climate-assist-vision/src/pages/Dashboard.js
+++ climate-assist-vision/src/pages/Dashboard.js
@@ -1,29 +1,77 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
 
-
 function Dashboard() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
- 
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
   const search = (evt) => {
     if (evt.key === "Enter") {
-      const url = `https://aerisweather1.p.rapidapi.com/observations/${query}`;
-  fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "aerisweather1.p.rapidapi.com",
-		"x-rapidapi-key": "659873ca62msh3d98a1c00ba332ap158787jsn182defbd3e10"
-	}
-})
-.then((res)=> res.json())
-.then((response) => {
-	console.log(response);
-  setWeather(response);
-  setQuery("");
-});
-}
-};
+      fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`, {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+          "x-rapidapi-key":
+            "659873ca62msh3d98a1c00ba332ap158787jsn182defbd3e10",
+        },
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(response);
+          setWeather(response);
+          setQuery("");
+        });
+    }
+  };
+
+  const fetchCoordinates = () =>
+    new Promise((resolve, reject) => {
+      if (!navigator.geolocation) {
+        return reject("Browser does not support geolocation API");
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        (location) => {
+          resolve(location.coords);
+          console.log(location);
+          setLatitude(location.coords.latitude);
+          setLongitude(location.coords.longitude);
+          console.log(latitude);
+          console.log(longitude);
+          const coordinates = latitude + "," + longitude;
+          fetch(
+            `https://weatherapi-com.p.rapidapi.com/current.json?q=${coordinates}`,
+            {
+              method: "GET",
+              headers: {
+                "x-rapidapi-host": "weatherapi-com.p.rapidapi.com",
+                "x-rapidapi-key":
+                  "659873ca62msh3d98a1c00ba332ap158787jsn182defbd3e10",
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((response) => {
+              console.log(response);
+              setWeather(response);
+              setQuery("");
+            });
+        },
+        (error) => {
+          switch (error.code) {
+            case "PERMISSION_DENIED":
+              return reject("Permission denied to get location");
+            case "TIMEOUT":
+              return reject("Timeout waiting for user response");
+            case "POSITION_UNAVAILABLE":
+            default:
+              return reject("Cannot detect user location");
+          }
+        }
+      );
+    });
 
   const dateBuilder = (dt) => {
     let months = [
@@ -69,19 +117,37 @@ function Dashboard() {
           onKeyPress={search}
         ></input>
       </div>
-      {(typeof weather.response != "undefined") ? (
-      <div>
-      <div className="location-box">
-        <div className="location">{weather.response.place.name}, {weather.response.place.country}</div>
-        <div className="date">{dateBuilder(new Date())}</div>
-      </div>
 
-      <div className="weather-box">
-        <div className="temperature">{Math.round(weather.response.ob.tempC)}°C</div>
-        <div className="weather">{weather.response.ob.weather}</div>
+      {typeof weather.location != "undefined" ? (
+        <div>
+          {weather.current.temp_c > 16 ? (
+            <div className="warm-backgorund"></div>
+          ) : (
+            <div className="cold-backgorund"></div>
+          )}
+          <div className="location-box">
+            <div className="location">
+              {weather.location.name}, {weather.location.country}
+            </div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+
+          <div className="weather-box">
+            <div className="temperature">
+              {Math.round(weather.current.temp_c)}°C
+            </div>
+            <div className="weather">{weather.current.condition.text}</div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="button-container">
+        <button className="current-location" onClick={fetchCoordinates}>
+          {" "}
+          Weather on current location{" "}
+        </button>
       </div>
-      </div>
-      ) : ('')}
     </div>
   );
 }
